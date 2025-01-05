ed9ede1536ca6cf92589160798a57292ec78f998
gustavo177 <gtvadf177@hotmail.com>
2025-01-05 16:03:48 -0500

    Modificando index.html

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Persona  
 
 # Create your views here.
 from django.http import HttpResponse
@@ -7,4 +8,8 @@ def hola_mundo(request):
     return HttpResponse("¡Hola, mundo desde Django!")
 
 def home(request):
-    return render(request, 'index.html')
+     # Cargar todos los datos de la tabla Persona
+    personas = Persona.objects.all()
+    
+    # Pasar los datos a la plantilla index.html
+    return render(request, 'index.html', {'personas': personas})
